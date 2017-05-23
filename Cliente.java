b79ea8a6e390a1b1d42cba5fe2d5d5909df6a7b9
Jonathas107 <jonathatiago@hotmail.com>
2017-05-23 13:43:48 -0300

    Adicionando cliente teste, definir outra classe só para compra depois.

--- Cliente.java
+++ Cliente.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package projeto;
+package rodolfo;
 
-/**
- *
- * @author antonio
- */
-class Cliente {
+import java.util.ArrayList;
+
+public class Cliente{
+    private String nome;
+    private String cpf;
+    private double divida = 0;
+    private double troco;
+    ArrayList<Estoque> estoque = new ArrayList();
     
+    public Cliente(String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getDivida() {
+        return divida;
+    }
+
+    public void setDivida(double divida) {
+        this.divida = divida;
+    }
+    public void PagarDivida(double valor){
+        if (valor > 0){
+            this.divida = this.divida - valor;
+            if (this.divida > 0){
+                this.troco = this.divida;
+                this.divida = 0;
+            }
+        }
+    }
+    public void Comprar(int codigo){
+        if (estoque.indexOf(codigo) != -1){
+            int indice = estoque.indexOf(codigo);
+            //estoque.set(estoque.indexOf(codigo), estoque.get(indice).quantidade - 1);
+            estoque.remove(codigo);
+        }
+    }
 }
